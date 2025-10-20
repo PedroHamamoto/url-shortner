576f0bb495927feca05d3e80714aea37daf50f95
PedroHamamoto <hamamoto.pedro@gmail.com>
2025-10-19 18:27:45 -0700

    project setup

---
+++ src/main/java/com/hamamoto/shortifier/ShortifierApplication.java
@@ -0,0 +1,13 @@
+package com.hamamoto.shortifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamamoto/shortifier/ShortifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamamoto.shortifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
